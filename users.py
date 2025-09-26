f104f03c4c929c7df9237a1fcc9b7cf4a255bba8
OclaZ <aslikhhamza2001@gamil.com>
2025-09-26 11:32:32 +0100

    users deleted from main

--- users.py
+++
@@ -1,7 +0,0 @@
-from data import utilisateurs
-
-def filter_utilisateur_adulte(users):
-    return [user for user in users if user[3] >= 18]
-
-adults = filter_utilisateur_adulte(utilisateurs)
-print(adults)
