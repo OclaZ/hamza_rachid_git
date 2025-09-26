a56f2ad584dbe5365a9e7950454656712f1eb088
OclaZ <aslikhhamza2001@gamil.com>
2025-09-26 12:46:42 +0100

    readme updated

--- Readme.md
+++ Readme.md
@@ -1,3 +1,6 @@
 # Projet
 
 [Enonce du projet](projet.pdf)
+
+
+made by *[OclaZ](https://www.github.com/OclaZ)*  and *[Rachid](https://www.github.com/rachidnaitsaid10)* 🙌
--- users.py
+++ users.py
@@ -8,11 +8,11 @@ def filter_utilisateur_adultes(users):
 def noms_en_majuscule(users):
     return users[1].upper() , users[2].upper()
 
-def dictionnaire_utilisateurs_livre(lst_user,lst_aime_livrea):
+def dictionnaire_utilisateurs_livre(lst_user,lst_aime_livre):
     dictio={}
     for (id_user, prenom, nom, age) in lst_user:
     
-        livres_aimes = [titre for (id_livre, titre) in lst_aime_livrea if id_livre == id_user]
+        livres_aimes = [titre for (id_livre, titre) in lst_aime_livre if id_livre == id_user]
         dictio[id_user] = {
             "nom_complet": f"{prenom.upper()} {nom.upper()}",
             "age": age,
