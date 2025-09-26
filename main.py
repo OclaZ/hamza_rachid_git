642f5044a1bce1612c9897ed6e2a56cd3140e36e
OclaZ <aslikhhamza2001@gamil.com>
2025-09-26 15:10:44 +0100

    main

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import data
-import books
-import users
+from data import utilisateurs
+from users import filter_utilisateur_adultes
 
-users=data.utilisateurs
-aime_livre=data.aime_livres
-livre=data.livres
 
-filtre_utilisateurs_adultes=users.filter_utilisateur_adultes
-noms_en_majuscule
+
+
+
