1b5cd49654c324bf3c08aed1aa9d931dbe27f377
rachidnaitsaid10 <rachidnaitsaid10@gmail.com>
2025-09-26 14:45:12 +0100

    Final Commit

--- books.py
+++ books.py
@@ -1,5 +1,6 @@
 import data
 
+aimeLivers = data.aime_livres
 Livres = data.livres
 Livres.sort(key=lambda x: x["année"], reverse=True)
 print(Livres)
@@ -10,6 +11,21 @@ def Récent_Ancien(MyList):
     Récent = Livres[0]
     print(Récent)
 
+def dectionaire(MyList):
+    list = []
+    dict = []
+
+    for aimeLiver in MyList:
+        list.append(aimeLiver[1])
+    print(list)
+
+    for i in list:
+        Count = list.count(i)
+        if { "id": i, "count": Count } not in dict:
+            dict.append({ "id": i, "count": Count })
+
+    print(dict)
 
 
 Récent_Ancien(Livres)
+dectionaire(aimeLivers)
