d57137538ddca187e5e9a1b583021cb7fb1f07f2
OclaZ <aslikhhamza2001@gamil.com>
2025-09-26 12:04:25 +0100

    add users.py

---
+++ users.py
@@ -0,0 +1,31 @@
+from data import utilisateurs
+from data import aime_livres
+
+def filter_utilisateur_adultes(users):
+        if users[3] >= 18:
+            return True
+
+def noms_en_majuscule(users):
+    return users[1].upper() , users[2].upper()
+
+def dictionnaire_utilisateurs_livre(lst_user,lst_aime_livrea):
+    dictio={}
+    for (id_user, prenom, nom, age) in lst_user:
+    
+        livres_aimes = [titre for (id_livre, titre) in lst_aime_livrea if id_livre == id_user]
+        dictio[id_user] = {
+            "nom_complet": f"{prenom.upper()} {nom.upper()}",
+            "age": age,
+            "livres": livres_aimes
+    }
+    return dictio
+   
+def afficha
+
+
+# adultes = list(filter(filter_utilisateur_adultes, utilisateurs))
+# print(adultes)
+# noms_maj=list(map(noms_en_majuscule, utilisateurs))
+# print(noms_maj)
+dictionnaire=dictionnaire_utilisateurs_livre(utilisateurs,aime_livres)
+print(dictionnaire)
